5de0654b079e552cd9df7464cece45828c493825
Sam <liweisen007@gmail.com>
2018-05-26 17:26:18 -0400

    add filehelper

--- app/src/main/java/com/samli/samli/MainActivity.java
+++ app/src/main/java/com/samli/samli/MainActivity.java
@@ -3,6 +3,12 @@ package com.samli.samli;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -11,14 +17,21 @@ import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText itemET;
+    private Button btn;
+    private ListView itemsList;
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         String api = "https://c.y.qq.com/v8/fcg-bin/fcg_v8_toplist_cp.fcg?g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=h5&needNewCode=1&tpl=3&page=detail&type=top&topid=36&_=1520777874472";
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
@@ -33,5 +46,25 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+        itemET = findViewById(R.id.item_edit_text);
+        btn = findViewById(R.id.add_btn);
+        itemsList = findViewById(R.id.items_list);
+
+        btn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add_btn:
+                String itemEntered = itemET.getText().toString();
+                adapter.add(itemEntered);
+                itemET.setText("");
+                Toast.makeText(this, "Item Added", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
