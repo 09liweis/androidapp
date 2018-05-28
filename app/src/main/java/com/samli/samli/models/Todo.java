d9ac8348fdbe951c3bb80708fa8901d8292cfdfe
Sam <liweisen007@gmail.com>
2018-05-27 20:12:26 -0400

    add Todo class

---
+++ app/src/main/java/com/samli/samli/models/Todo.java
@@ -0,0 +1,13 @@
+package com.samli.samli.models;
+
+public class Todo {
+    String name;
+    String _id;
+    String status;
+
+    public Todo(String _id, String name, String status) {
+        this._id = _id;
+        this.name = name;
+        this.status = status;
+    }
+}
