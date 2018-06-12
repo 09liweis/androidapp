5101f6b9c05dcff330713628c60a0d5d44fb4d7d
Sam <liweisen007@gmail.com>
2018-06-11 23:20:07 -0400

    implement getter setter

--- app/src/main/java/com/samli/samli/models/Transaction.java
+++ app/src/main/java/com/samli/samli/models/Transaction.java
@@ -6,4 +6,48 @@ public class Transaction {
     String price;
     String date;
     String category;
+
+    public Transaction() {
+
+    }
+
+    public String getId() {
+        return _id;
+    }
+
+    public void setId(String id) {
+        this._id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
