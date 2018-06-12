5b15ef788c23946e9e81121009cd8733bfbb227f
Sam <liweisen007@gmail.com>
2018-06-11 21:45:50 -0400

    add RecyclerView property

---
+++ app/src/main/java/com/samli/samli/adapters/TransactionsAdapter.java
@@ -0,0 +1,4 @@
+package com.samli.samli.adapters;
+
+public class TransactionsAdapter {
+}
--- app/src/main/java/com/samli/samli/fragments/TransactionsFragment.java
+++ app/src/main/java/com/samli/samli/fragments/TransactionsFragment.java
@@ -2,6 +2,7 @@ package com.samli.samli.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,8 @@ import com.samli.samli.R;
  * create an instance of this fragment.
  */
 public class TransactionsFragment extends Fragment {
+    private RecyclerView transactionsView;
+
 
     public TransactionsFragment() {
         // Required empty public constructor
