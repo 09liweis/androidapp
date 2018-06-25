f2c6fcf22010bf09bf268357b69ad20da482f9c8
Sam <liweisen007@gmail.com>
2018-06-24 21:21:32 -0400

    remove newinstance arguement

--- app/src/main/java/com/samli/samli/fragments/MusicFragment.java
+++ app/src/main/java/com/samli/samli/fragments/MusicFragment.java
@@ -43,11 +43,9 @@ public class MusicFragment extends Fragment {
      * @return A new instance of fragment MusicFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static MusicFragment newInstance(String param1, String param2) {
+    public static MusicFragment newInstance() {
         MusicFragment fragment = new MusicFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
