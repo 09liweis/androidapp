f69e581599b0650161ffb30654781ae1fdb47f46
Sam <liweisen007@gmail.com>
2018-06-27 19:17:46 -0400

    remove unuse method

--- app/src/main/java/com/samli/samli/fragments/MusicFragment.java
+++ app/src/main/java/com/samli/samli/fragments/MusicFragment.java
@@ -50,24 +50,4 @@ public class MusicFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_music, container, false);
     }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
