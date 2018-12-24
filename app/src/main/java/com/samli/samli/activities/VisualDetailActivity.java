8b6f5a9059d99270dd35bda30270ce9c85fb5266
Sam <liweisen007@gmail.com>
2018-12-24 00:41:42 -0500

    add intent on detail to form

--- app/src/main/java/com/samli/samli/activities/VisualDetailActivity.java
+++ app/src/main/java/com/samli/samli/activities/VisualDetailActivity.java
@@ -44,8 +44,10 @@ public class VisualDetailActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent;
+                intent = new Intent(VisualDetailActivity.this, VisualFormActivity.class);
+                intent.putExtra("doubanId", visual.getDoubanId());
+                startActivity(intent);
             }
         });
 
