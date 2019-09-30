3c5cb20d0ec9c0cf5c8e91033cb733af42c79e26
Sam Li <weisen.li@hotmail.com>
2019-09-30 11:15:42 +0800

    add empty intent id for new todo

--- app/src/main/java/com/samli/samli/fragments/TodoListFragment.java
+++ app/src/main/java/com/samli/samli/fragments/TodoListFragment.java
@@ -78,6 +78,7 @@ public class TodoListFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getContext(), TodoFormActivity.class);
+                intent.putExtra("id", "");
                 startActivity(intent);
             }
         });
