57fe9da774b8f330add7a689ecb9d5d786d23791
Sam Li <weisen.li@hotmail.com>
2019-10-08 18:25:52 +0800

    add showToast method to Util class

--- app/src/main/java/com/samli/samli/models/Util.java
+++ app/src/main/java/com/samli/samli/models/Util.java
@@ -1,4 +1,10 @@
 package com.samli.samli.models;
 
+import android.content.Context;
+import android.widget.Toast;
+
 public class Util {
+  public static void showToast(Context c,String msg) {
+    Toast.makeText(c, msg, Toast.LENGTH_SHORT).show();
+  }
 }
