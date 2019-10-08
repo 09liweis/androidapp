8814004e06e99702438735376a1fc100e45356f8
Sam Li <weisen.li@hotmail.com>
2019-10-08 20:59:07 +0800

    fix indent

--- app/src/main/java/com/samli/samli/adapters/TodoListAdapter.java
+++ app/src/main/java/com/samli/samli/adapters/TodoListAdapter.java
@@ -16,55 +16,55 @@ import java.util.ArrayList;
 
 public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.DataViewHolder> {
 
-    private Context mContext;
-    private ArrayList<Todo> todoList;
+  private Context mContext;
+  private ArrayList<Todo> todoList;
 
-    public TodoListAdapter(Context mContext, ArrayList<Todo> todoList) {
-        this.mContext = mContext;
-        this.todoList = todoList;
-    }
+  public TodoListAdapter(Context mContext, ArrayList<Todo> todoList) {
+    this.mContext = mContext;
+    this.todoList = todoList;
+  }
 
-    @Override
-    public DataViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(mContext).inflate(R.layout.todo,null);
-        //使用代码设置宽高（xml布局设置无效时）
-        view.setLayoutParams(new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT));
-        final DataViewHolder holder = new DataViewHolder(view);
-        holder.itemView.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Integer pos = holder.getAdapterPosition();
-                Todo todo = todoList.get(pos);
-                Intent intent = new Intent(mContext.getApplicationContext(), TodoFormActivity.class);
-                intent.putExtra("id", todo.getId());
-                mContext.startActivity(intent);
-            }
-        });
-        return holder;
-    }
+  @Override
+  public DataViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    View view = LayoutInflater.from(mContext).inflate(R.layout.todo,null);
+    //使用代码设置宽高（xml布局设置无效时）
+    view.setLayoutParams(new ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT));
+    final DataViewHolder holder = new DataViewHolder(view);
+    holder.itemView.setOnClickListener(new View.OnClickListener(){
+      @Override
+      public void onClick(View view) {
+        int pos = holder.getAdapterPosition();
+        Todo todo = todoList.get(pos);
+        Intent intent = new Intent(mContext.getApplicationContext(), TodoFormActivity.class);
+        intent.putExtra("id", todo.getId());
+        mContext.startActivity(intent);
+      }
+    });
+    return holder;
+  }
 
-    @Override
-    public void onBindViewHolder(DataViewHolder holder, int position) {
-        Todo todo = todoList.get(position);
-        holder.todoName.setText(todo.getName() + " - " + todo.getDate());
-        holder.todoStatus.setText(todo.getStatus());
-    }
+  @Override
+  public void onBindViewHolder(DataViewHolder holder, int position) {
+    Todo todo = todoList.get(position);
+    holder.todoName.setText(todo.getName() + " - " + todo.getDate());
+    holder.todoStatus.setText(todo.getStatus());
+  }
 
-    @Override
-    public int getItemCount() {
-        return todoList.size();
-    }
+  @Override
+  public int getItemCount() {
+    return todoList.size();
+  }
 
-    public static class DataViewHolder extends RecyclerView.ViewHolder{
-        TextView todoName;
-        TextView todoStatus;
-        public DataViewHolder(View itemView) {
-            super(itemView);
-            todoName = itemView.findViewById(R.id.todo_name);
-            todoStatus = itemView.findViewById(R.id.todo_status);
-        }
+  public static class DataViewHolder extends RecyclerView.ViewHolder{
+    TextView todoName;
+    TextView todoStatus;
+    public DataViewHolder(View itemView) {
+      super(itemView);
+      todoName = itemView.findViewById(R.id.todo_name);
+      todoStatus = itemView.findViewById(R.id.todo_status);
     }
+  }
 }
 
